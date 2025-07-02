0743bc685103a0e05e9a323ea9b8b02db5c8af76
Sakar Bhandari <bhandarisakar@outlook.com>
2025-07-02 12:39:47 +0545

    Requirements Files Initialized

--- billing/management/commands/send_unpaid_bill_alerts.py
+++ billing/management/commands/send_unpaid_bill_alerts.py
@@ -2,12 +2,13 @@ from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
 from django.conf import settings
 from billing.models import Bill
-
+from django.db import models
 class Command(BaseCommand):
     help = 'Send fortnightly alert for unpaid bills'
 
     def handle(self, *args, **kwargs):
-        unpaid_bills = Bill.objects.filter(is_fully_paid=False)
+        # unpaid_bills = Bill.objects.filter(is_fully_paid=False)
+        unpaid_bills = Bill.objects.filter(total_amount__gt=models.F('amount_paid'))
         if unpaid_bills.exists():
             message_lines = [
                 f"Unpaid Bill Alert (Fortnightly Summary)",
@@ -18,14 +19,19 @@ class Command(BaseCommand):
                 outstanding = bill.total_amount - bill.amount_paid
                 message_lines.append(
                     f"Bill #{bill.id} | Car: {bill.service_record.car.plate_number} | "
-                    f"Outstanding: {outstanding}"
+                    f"Created on: {bill.created_at.date()} | "
+                    f"Total Amount: {bill.total_amount} | "
+                    f"Amount Paid: {bill.amount_paid} | "
+                    f"Remaining: {outstanding} | "
+                    f"Owner Name: {bill.service_record.car.owner_name} | "
+                    f"Owner Contact Number: {bill.service_record.car.owner_contact} |"
                 )
             
             send_mail(
                 subject="Fortnightly Unpaid Bill Summary",
                 message="\n".join(message_lines),
                 from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=[settings.ACCOUNTS_TEAM_EMAIL],
+                recipient_list=["axortechnp@gmail.com"],
                 fail_silently=False
             )
             self.stdout.write(self.style.SUCCESS("Fortnightly unpaid bill alert sent."))
--- billing/tasks.py
+++ billing/tasks.py
@@ -1,12 +1,6 @@
-# from celery import shared_task
-# from django.core.management import call_command
-
-# @shared_task
-# def send_fortnightly_unpaid_bill_alerts():
-#     call_command('send_unpaid_bill_alerts')
 from celery import shared_task
+from django.core.management import call_command
 
 @shared_task
-def test_celery_task():
-    print("✅ Celery task ran successfully!")
-    return "Done"
+def send_fortnightly_unpaid_bill_alerts():
+    call_command('send_unpaid_bill_alerts')
